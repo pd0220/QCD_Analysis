5cfe666061163746a1a245600c46a27c9a879be1
pd0220 <david.pesznyak@gmail.com>
2021-02-01 16:56:22 +0100

    continuum limes updates, new linalg structure

--- ContinuumLimes1D.cpp
+++ ContinuumLimes1D.cpp
@@ -26,8 +26,10 @@ int main(int argc, char **argv)
     }
 
     // reading argument list
+    //
     // fixing temperature value
     std::string const TString = argv[1];
+
     // where to cut baryon numbers (B = 2 or 3)
     int const baryonCut = std::atoi(argv[2]);
     std::string const baryonCutString = argv[2];
@@ -37,35 +39,50 @@ int main(int argc, char **argv)
         std::exit(-2);
     }
 
+    // number of coefficients
+    int coeffNum = 0;
+    if (baryonCut == 2)
+        coeffNum = 12;
+    else
+        coeffNum = 16;
+
     // file specifiers
     // rule: smallest lattice goes FIRST
     std::vector<std::string> dataNames{
-        "T" + TString + ".24x8.B" + baryonCutString,
-        "T" + TString + ".32x10.B" + baryonCutString,
         "T" + TString + ".36x12.B" + baryonCutString,
-        "T" + TString + ".48x16.B" + baryonCutString,
-    };
+        "T" + TString + ".32x10.B" + baryonCutString,
+        "T" + TString + ".24x8.B" + baryonCutString};
     // number of different lattice sizes
-    int const numLattice = static_cast<int>(dataNames.size());
+    int const latticeNum = static_cast<int>(dataNames.size());
+
+    // data points for the linear fit
+    Eigen::VectorXd Nt(3);
+    Nt << 1. / 144., 1. / 100., 1. / 64.;
+
+    // container for estimated sector coefficients
+    std::vector<Eigen::VectorXd> SectorCoeffsVector(coeffNum);
+    // container for sector coefficient square errors (jackknife)
+    std::vector<Eigen::VectorXd> SectorErrsSqVector(coeffNum);
+    // set vector sizes and values (to zero)
+    for (int iVec = 0; iVec < coeffNum; iVec++)
+    {
+        SectorCoeffsVector[iVec] = Eigen::VectorXd::Zero(latticeNum);
+        SectorErrsSqVector[iVec] = Eigen::VectorXd::Zero(latticeNum);
+    }
 
     // reading files
-    // container for estimated sector coefficients
-    std::vector<Eigen::VectorXd> SectorCoeffsVector(numLattice);
-    // container for sector coefficient square errors (jackknife)
-    std::vector<Eigen::VectorXd> SectorErrsSqVector(numLattice);
-    for (int iFile = 0; iFile < numLattice; iFile++)
+    for (int iFile = 0; iFile < latticeNum; iFile++)
     {
         // raw data matrix
         Eigen::MatrixXd RawDataMat = ReadFile(FileName(dataNames[iFile]));
-        // sector coefficient
-        SectorCoeffsVector[iFile] = RawDataMat.col(0);
-        // sector coefficient error
-        SectorErrsSqVector[iFile] = Eigen::VectorXd::Zero(RawDataMat.rows());
-        for (int iRow = 0; iRow < RawDataMat.rows(); iRow++)
+
+        // saving coefficients at different temperatures
+        for (int iCoeff = 0; iCoeff < coeffNum; iCoeff++)
         {
-            SectorErrsSqVector[iFile](iRow) = JCKVariance(RawDataMat.row(iRow).segment(1, RawDataMat.cols() - 1));
+            SectorCoeffsVector[iCoeff](iFile) = RawDataMat(iCoeff, 0);
+            SectorErrsSqVector[iCoeff](iFile) = JCKVariance(RawDataMat.row(iCoeff).segment(1, RawDataMat.cols() - 1));
         }
     }
 
-    
+    std::cout << SectorCoeffsVector[0] << std::endl;
 }
